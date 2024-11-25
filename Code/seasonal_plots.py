00fa1dd4e6d16cbffda6011806c370c87387a39c
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2024-11-25 13:01:36 -0500

    name change

--- Code/seasonal_plots.py
+++ Code/seasonal_plots.py
@@ -17,11 +17,11 @@ for v in phy_vars:
     for ax, dr in zip(axes, depth_ranges):
         err = f'{v}_STD_{dr[0]}-{dr[1]}dbar'
         varname = f'{v}_{dr[0]}-{dr[1]}dbar'
-        sns.scatterplot(data=df, x='dayofyear', y=varname, hue='year', ax=ax, legend=False, linewidth=0.1, s=0.8, zorder=2)
+        g = sns.scatterplot(data=df, x='dayofyear', y=varname, hue='year', ax=ax, legend=False, linewidth=0.1, s=0.8, zorder=2)
         ax.set_ylabel(v)
         ax.set_title(f'{dr[0]} - {dr[1]} dbar', loc='left')
     
     fig.tight_layout()
     fig.set_size_inches(fig.get_figwidth()/3, fig.get_figheight())
-    fig.savefig(f'../Figures/argo/{v}_yearday.png', dpi=350, bbox_inches='tight')
+    fig.savefig(f'../Figures/argo/{v}_single_year.png', dpi=350, bbox_inches='tight')
     plt.close(fig)
