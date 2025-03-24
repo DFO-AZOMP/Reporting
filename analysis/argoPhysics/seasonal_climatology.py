ca31dbd9a2e60e490d21586d913cc1df54261a82
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2025-03-24 13:31:20 -0400

    better commenting

--- analysis/argoPhysics/seasonal_climatology.py
+++ analysis/argoPhysics/seasonal_climatology.py
@@ -42,6 +42,7 @@ monthly_means = ix.groupby(['year', 'month'])[varnames].mean().unstack()
 clim = monthly_means.loc[range(ix.year.min(), clim_year+1)].mean()
 std = monthly_means.loc[range(ix.year.min(), clim_year+1)].std()
 
+# axis and title labels
 labels = {
     'TEMP':f'T ({chr(176)}C)',
     'PSAL':'SP',
@@ -67,30 +68,35 @@ for v in phy_vars:
         t = pd.Series([pd.Timestamp(year=y, month=m, day=1) for y in [2023, 2024] for m in range(1, 13)])
         td = pd.Timedelta(days=15)
 
+        # double up climatology for 2 year figure
         c = pd.concat((clim.loc[varname], clim.loc[varname]))
         s = pd.concat((std.loc[varname], std.loc[varname]))
         ax.plot(t+td, c, linewidth=3, label='climatology')
         ax.fill_between(t+td, c-s, c+s, alpha=0.3, label=None)
 
+        # alaysis years monthly means
         p = monthly_means.loc[(2023, 2024), varname].stack()
         ax.plot(t+td, p, linewidth=3, label=f'{years_of_interest[0]}-{years_of_interest[1]}')
 
+        # deltas
         delta = p.values - c.values
         colors = (delta / np.max(np.abs(delta)) + 1)/2
 
+        # colorbar along the bottom for deltas
         cmap = ListedColormap([cmo.cm.balance(c) for c in colors])
         _, fax = plt.subplots()
         fpc = fax.pcolormesh(colors.reshape(6, 4), cmap=cmap)
-
         cax = fig.add_axes([0.125, 0.02, 0.777, 0.045])
         cb = plt.colorbar(fpc, cmap=cmap, orientation='horizontal', cax=cax)
         cb.ax.set_xlabel('$\Delta$' + labels[v])
 
+        # format xticks and colorbar ticks
         ax.set_xticks(t)
         # hide major tick labels
         ax.set_xticklabels('')
         ax.legend(loc=4, fontsize=10)
 
+        # delta values in colorbar cell
         cb.ax.get_xaxis().set_ticks([])
         for i, lab in enumerate([f'{d:.2f}' for d in delta]):
             text_color = 'white' if colors[i] > 0.75 or colors[i] < 0.25 else 'black'
@@ -104,8 +110,10 @@ for v in phy_vars:
         ax.axvline(pd.Timestamp(year=2024, month=1, day=1), color='black')
         ax.set_xlim((t.min(), t.max()+pd.Timedelta(weeks=4)))
 
+        # titles and axis labels
         ax.set_title(f'Mean {names[v]}, {d[0]}-{d[1]} dbar', loc='left', fontweight='bold')
         ax.set_ylabel(labels[v])
 
+        # save figure
         fig.savefig(f'figures/{years_of_interest[1]}/timeseries/{varname}_2year_seasonal_cycle.png', bbox_inches='tight', dpi=350)
         plt.close('all')
